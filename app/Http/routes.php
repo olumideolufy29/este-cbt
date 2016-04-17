cf65c2b4f88ac7167e333cea1fad35095aadcb9b
Firdauzfan <firdauzfanani@gmail.com>
2016-04-17 08:04:27 +0700

    computerBasedtest

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/DashboardSiswa', function () {
+    return view('DashboardSiswa');
 });
---
+++ resources/views/dashboardsiswa.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Selamat Datang</title>
+
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/style2.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="css/responsive.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+	<section class="main-section" id="Tentang"><!--main-section-start-->
+    <div class="container">
+        <h2>Selamat Datang</h2>
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 wow fadeInLeft delay-05s">
+                <center><img src="img/profil.png"></center>
+                <div class="profil">
+                <a href="#"><h2 style="color:white;margin-top:13px;">Profil</h2></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-6 wow fadeInLeft delay-05s">
+                <center><img src="img/ujian.png"></center>
+                <a href="#"><h2 style="color:white;margin-top:10px">Ujian</h2></a>
+            </div>
+        
+        </div>
+    </div>
+</section>
+</body>
+</html>
